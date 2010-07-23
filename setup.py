3c3c55dc16e7bc5c8e54873759a4e0c56c10ea79
Lucian Langa <lucilanga@gnome.org>
2010-07-23 12:24:01 +0300

    update pkg description

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ import glob
 from distutils.core import setup
 
 setup(name='gnoduino',
-	version='1.0',
+	version='0.1.0',
+	description='Gnome Arduino IDE implementation',
 	package_dir={'gnoduino': 'src'},
 	packages = ['gnoduino'],
 	scripts = ['gnoduino'],
+
+	author='Lucian Langa',
+	author_email='lucilanga@gnome.org',
+	url='http://gnome.eu.org/evo/index.php/Gnoduino',
+	license='GPL',
+
 	data_files = [('share/gnoduino/ui', ['ui/main.ui']),
 			('share/gnoduino/pixmaps', glob.glob('pixmaps/*.png'))
 	]
